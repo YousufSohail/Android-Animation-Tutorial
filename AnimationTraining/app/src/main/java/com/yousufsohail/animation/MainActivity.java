817d7e1e04e7194d7a85549cabe59ac56256f999
Yousuf Sohail <ysohail@folio3.com>
2016-04-14 11:32:24 +0500

    [Tutorial 1: Introduction] Basic example of View Animations

--- AnimationTraining/app/src/main/java/com/yousufsohail/animation/MainActivity.java
+++ AnimationTraining/app/src/main/java/com/yousufsohail/animation/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yousufsohail.animation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void imageClicked(View thumbnailView) {
+        ImageView thumbnail = (ImageView) thumbnailView;
+        ImageView animatedImage = (ImageView) findViewById(R.id.animatedImage);
+
+        assert animatedImage != null;
+        animatedImage.setImageDrawable(thumbnail.getDrawable());
+        animatedImage.setVisibility(View.VISIBLE);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale_animation01);
+        animatedImage.startAnimation(animation);
     }
 }
